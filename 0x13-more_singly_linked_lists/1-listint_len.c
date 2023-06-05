040b0e9f90468bdb553a5eb46c1dc04d207c84ea
rehtech142 <boikanyosehume@gmail.com>
2023-06-05 13:12:11 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,9 +2,9 @@
 
 /**
  * listint_len - outputs the number of elements in the  linked lists
- * @h: the linked list of a type listint_t to pass over
+ * @h: the linked list of a type listint_t to be passed over
  *
- * output: the number of nodes
+ * Return: the number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
